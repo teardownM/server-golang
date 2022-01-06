a1c1188f521c25d0cbe0a3120002d68f5b323c4e
root <root@sledgeMP-server-dev>
2022-01-06 22:26:41 +0000

    Add build script

---
+++ scripts/build_latest.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+cd ./TeardownNakamaServer
+
+if [ $# -eq 0 ]
+  then
+    echo "WARNING: RUNNING THIS LOCALLY WILL HARD RESET YOUR CURRENT WORKING DIRECTORY TO MAIN!"
+    echo "Add --yes to confirm"
+    exit 1
+fi
+
+
+DIR="modules/"
+if [ ! -d "$DIR" ]; then
+  # Take action if $DIR exists. #
+  echo "Creating module folder"
+  mkdir modules
+fi
+
+git reset --hard origin/main
+git pull
+
+cd ./src
+docker run --rm -w "/builder" -v "${PWD}:/builder" heroiclabs/nakama-pluginbuilder:3.10.0 build -buildmode=plugin -trimpath -o ./modules/teardownNK.so
+
+mv ./modules/* ../modules
+rm -r ./modules/
+
+cd ..
+
+docker restart 2e194d1c49a0
+
+echo "Successfully built and restarted Nakama"
